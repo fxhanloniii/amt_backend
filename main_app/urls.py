e612484ac5bb4f7a48abe86d20d463d384b19b67
fxhanloniii <hanlonfrankiex@gmail.com>
2023-08-31 14:38:39 -0700

    views/url

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+from main_app.views import ItemViewSet
+
+router = DefaultRouter()
+router.register(r'items', ItemViewSet)
 
 urlpatterns = [
-    path('', views.Home, name='home'),
+    path('', views.Home.as_view(), name='home'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+from .models import Item
+from .serializers import ItemSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
-def Home(request):
-    return HttpResponse('<h1>Hello World</h1>')
+class Home(APIView):
+    def get(self, request):
+        data = {
+            "message": "Hello World"
+        }
+        return Response(data)
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
