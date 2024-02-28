9308c3b3dcc948bae79d0d9edeea1800e05bd451
fxhanloniii <hanlonfrankiex@gmail.com>
2024-02-28 14:19:46 -0800

    edit host

--- amt_project/settings.py
+++ amt_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.57.40.111']
+ALLOWED_HOSTS = ['3.101.60.200']
 
 
 # Application definition
