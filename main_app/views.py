7630745d3c926f43c76714cba805514f29e6e92b
fxhanloniii <hanlonfrankiex@gmail.com>
2024-11-12 16:25:34 -0800

    backend updates

--- main_app/views.py
+++ main_app/views.py
@@ -44,7 +44,7 @@ class Home(APIView):
         return Response(data)
     
 class CustomPasswordResetView(PasswordResetView):
-    email_template_name = 'templates/registration/reset_password.html'
+    email_template_name = 'registration/nonexistent_template.html'
 
 class CustomRegisterView(RegisterView):
     serializer_class = CustomRegisterSerializer
